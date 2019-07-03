8db2b79e34b4c82e38e2b9fcce6c5e227105e33c
Olha Vasylyshyn <olickav@gmail.com>
2019-07-03 14:42:30 +0200

    gitignore for cmake

--- Project/main.cpp
+++ Project/main.cpp
@@ -4,7 +4,7 @@
 #include "Histogram.h"
 
 #include <experimental/filesystem>
-#define PRPATH fs::current_path()//.parent_path()
+#define PRPATH fs::current_path().parent_path()
 #define POS en-1
 namespace fs = std::experimental::filesystem;
 
